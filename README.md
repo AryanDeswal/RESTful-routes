e08d387a591643308def83380d373298ade4df94
Aryan Deswal <aryandeswal870@gmail.com>
2023-04-28 20:57:37 +0530

    commit-1

---
+++ README.md
@@ -0,0 +1,9 @@
+# RESTful-routes 
+# Name        Path                Verb            Purpose
+Index      /comments             GET        Displayy all comments
+New        /comments/new         GET        Form to create new comments
+Create     /comments             POST       Create new comments on server
+Show       /comments/:id         GET        Details for one specific comment
+Edit       /comments/:id/edit    GET        Formt to edit specific comment
+Update     /comments/:id         PATCH      Updates specific comments on server 
+Destroy    /comments/:id         DELETE     Deletes specific item on server
---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+let comments = [
+    {
+        id: uuidv4(),
+        username: 'Todd',
+        comment: 'lol that is so funny'
+    },
+    {
+        id: uuidv4(),
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog.'
+    },
+    {
+        id: uuidv4(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        id: uuidv4(),
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuidv4() });
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
+});
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
